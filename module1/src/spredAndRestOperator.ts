dcac68ba581148259cb3a956c97c117280082913
developerMasum <developer.masum2001@gmail.com>
2023-11-02 23:57:33 +0600

    rest op added

--- module1/src/spredAndRestOperator.ts
+++ module1/src/spredAndRestOperator.ts
@@ -5,3 +5,8 @@
 const home1:string[]= ['a','b','c','d'];
 const home2:string[] = ['f','g','h','l','y'];
 home1.push(...home2)
+
+// rest op
+const friendsGreetings = (...friends:string[])=>{
+   friends.forEach(friend=> console.log(`hi ${friend}`)) 
+}
